82b61fa33c6df87587672e5f7bb03fe831b8ac5e
jaycer95 <jacer.dabbabi95@gmail.com>
2020-06-25 15:35:23 +0000

    4th task

--- models/base_model.py
+++ models/base_model.py
@@ -8,31 +8,42 @@ import datetime
 
 class BaseModel:
 
-    """ define all common attributes"""
+        """ define all common attributes"""
 
-    def __init__(self, *args, **kwargs):
-        """ instantination"""
+            def __init__(self, *args, **kwargs):
+                        """ instantination"""
 
-        self.id = str(uuid.uuid4())
-        self.created_at = datetime.datetime.now()
-        self.updated_at = None
+                                if kwargs is not {}:
+                                                self.id = str(uuid.uuid4())
+                                                            self.created_at = datetime.datetime.now()
+                                                                        self.updated_at = datetime.datetime.now()
+                                                                                    for k , v in kwargs.items():
+                                                                                                        if k is not "__class__":
+                                                                                                                                if v is kwargs["created_at"] or v is kwargs["updated_at"]:
+                                                                                                                                                            v = datetime.datetime.strptime(v, "%Y-%m-%dT%H:%M:%S.%f")
+                                                                                                                                                                                setattr(self, k, v)
+                                                                                                                                                                                        else:
+                                                                                                                                                                                                        self.id = str(uuid.uuid4())
+                                                                                                                                                                                                                    self.created_at = datetime.datetime.now()
 
-    def __str__(self):
-        """ returns a string object"""
 
-        return "[{}] ({}) {}".format(self.__class__.__name__,
-                                     self.id, self.__dict__)
+                                                                                                                                                                                                                        def __str__(self):
+                                                                                                                                                                                                                                    """ returns a string object"""
 
-    def save(self):
-        """ updates the public instance attribute"""
+                                                                                                                                                                                                                                            return "[{}] ({}) {}".format(self.__class__.__name__,
+                                                                                                                                                                                                                                                                                                              self.id, self.__dict__)
 
-        self.updated_at = datetime.datetime.now()
+                                                                                                                                                                                                                                                def save(self):
+                                                                                                                                                                                                                                                            """ updates the public instance attribute"""
 
-    def to_dict(self):
-        """ returns a dictionary containing all key/values of __dict__"""
+                                                                                                                                                                                                                                                                    self.updated_at = datetime.datetime.now()
 
-        dict = self.__dict__
-        self.created_at = self.created_at.strftime("%Y-%m-%dT%H:%M:%S.%f")
-        self.updated_at = self.updated_at.strftime("%Y-%m-%dT%H:%M:%S.%f")
-        dict.update({'__class__': self.__class__.__name__})
-        return dict
+                                                                                                                                                                                                                                                                        def to_dict(self):
+                                                                                                                                                                                                                                                                                    """ returns a dictionary containing all key/values of __dict__"""
+
+                                                                                                                                                                                                                                                                                            dict = self.__dict__
+                                                                                                                                                                                                                                                                                                    self.created_at = self.created_at.strftime("%Y-%m-%dT%H:%M:%S.%f")
+                                                                                                                                                                                                                                                                                                            self.updated_at = self.updated_at.strftime("%Y-%m-%dT%H:%M:%S.%f")
+                                                                                                                                                                                                                                                                                                                    dict.update({'__class__': self.__class__.__name__})
+                                                                                                                                                                                                                                                                                                                            return dict
+                                                                                                                                                                                                                                                                                                                            
