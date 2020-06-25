d97ac6a8caee08a722e054f2193e012d0db23a4d
jaycer95 <jacer.dabbabi95@gmail.com>
2020-06-25 13:40:02 +0000

    Super

--- models/base_model.py
+++ models/base_model.py
@@ -10,7 +10,7 @@ class BaseModel:
 
     """ define all common attributes"""
 
-    def __init__(self):
+    def __init__(self, *args, **kwargs):
         """ instantination"""
 
         self.id = str(uuid.uuid4())
@@ -26,12 +26,11 @@ class BaseModel:
     def save(self):
         """ updates the public instance attribute"""
 
-        super().__init__()
         self.updated_at = datetime.datetime.now()
 
     def to_dict(self):
         """ returns a dictionary containing all key/values of __dict__"""
-        super().__init__()
+
         dict = self.__dict__
         self.created_at = self.created_at.strftime("%Y-%m-%dT%H:%M:%S.%f")
         self.updated_at = self.updated_at.strftime("%Y-%m-%dT%H:%M:%S.%f")
