1cfc82a5cc57c0344d477e1f024c3eecbb1f439a
nourouhichi <1417@holbertonschool.com>
2020-07-01 10:53:09 +0100

    new tgry

--- console.py
+++ console.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
 """
+New  module
 """
 
 
@@ -7,22 +8,23 @@ import sys
 import cmd
 import models
 from models.base_model import BaseModel
-class HBNBCommand(cmd.Cmd):
-    prompt = "(hbnb)"
-    __allclasses= ["BaseModel", "FileStorage"]
 
+
+class HBNBCommand(cmd.Cmd):
+    """ a command line class """
+
+    prompt = "(hbnb)"
 
     def emptyline(self):
         """doesn't execute anything when empty line"""
         pass
-    
+
     def do_EOF(self, line):
         """exits the program by returning True"""
         return True
 
     def do_quit(self, line):
         """ exits the program when entering the command quit"""
-        quit()    
-    
+        return True
 if __name__ == '__main__':
     HBNBCommand().cmdloop()
