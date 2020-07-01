ce5f1e4d7945b5575c8c4ca8e37141be01a1925d
nourouhichi <1417@holbertonschool.com>
2020-07-01 11:01:04 +0100

    new add

--- models/base_model.py
+++ models/base_model.py
@@ -14,7 +14,7 @@ class BaseModel:
     def __init__(self, *args, **kwargs):
         """ instantination"""
 
-        if kwargs is not {} and kwargs is not None:
+        if kwargs is not {}:
             self.id = str(uuid.uuid4())
             self.created_at = datetime.datetime.now()
             self.updated_at = datetime.datetime.now()
