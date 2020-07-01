2594bc5fde6fa68ac34f061f19ce870270fe5369
nourouhichi <1417@holbertonschool.com>
2020-07-01 10:41:27 +0100

    new tgry

--- console.py
+++ console.py
@@ -22,56 +22,7 @@ class HBNBCommand(cmd.Cmd):
 
     def do_quit(self, line):
         """ exits the program when entering the command quit"""
-        quit()
-
-    """def do_create(self):
-        """ creates a new instance from an existant class"""
-        if sys.argv[1] is None:
-            print("** class name missing **")
-        elif sys.argv[1] not in HBNBCommand.__allclasses:
-            print("** class doesn't exist **")
-        else:
-            new_inst = eval(sys.argv[1])()
-            new_inst.save()
-            print(new_inst.id)
-    
-    def do_show(self):
-        """ prints the str representation of an existant object from an existant class"""
-        if sys.argv[1] is None:
-            print("** class name missing **")
-        elif sys.argv[1] not in HBNBCommand.__allclasses:
-            print("** class doesn't exist **")
-        elif sys.argv[2] is None:
-            print("** instance id missing **")
-        for k, v in models.storage.all().items:
-            if v.id is sys.argv[2] and v.__class__.__name__ is sys.argv[1]:
-                print(v)
-                return
-        print("** no instance found **")
-
-    def do_destroy(self):
-        """deletes an existant instance from an existant class """
-        if sys.argv[1] is None:
-            print("** class name missing **")
-        elif sys.argv[1] not in HBNBCommand.__allclasses:
-            print("** class doesn't exist **")
-        elif sys.argv[2] is None:
-            print("** instance id missing **")
-        for k, v in models.storage.all().items:
-            if v.id is sys.argv[2] and v.__class__.__name__ is sys.argv[1]:
-                del(models.storage.all()[k])
-                models.storage.save()
-                return
-        print("** no instance found **")
-
-    def do_all(self):
-        """ """"""
-        
-
-
-        
-
-    
+        quit()    
     
 if __name__ == '__main__':
     HBNBCommand().cmdloop()
